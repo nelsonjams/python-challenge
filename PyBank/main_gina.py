f44e13c68ff4ca3d89fe7d9c71f8484b29b97537
nelsonjams <56487708+nelsonjams@users.noreply.github.com>
2020-01-14 18:59:10 -0500

    Add files via upload

---
+++ PyBank/main_gina.py
@@ -0,0 +1,58 @@
+import os
+import csv
+ #set variables for calculations
+total_pnl = 0
+total_months = 0
+max_pnl_increase = 0
+max_pnl_decrease = 999999
+max_pnl_increase_month = ""
+max_pnl_decrease_month = ""
+total_pnl_change = []
+   
+
+budget_csv= os.path.join('Resources', 'budget_data.csv')
+   
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #skip the header using next
+    csv_header = next(csvreader)
+    print(f"Header: {csv_header}")
+    first_row_data = next(csvreader)
+    first_month_pnl = int(first_row_data[1])
+    total_months = 1
+    print(first_month_pnl)
+    total_pnl = total_pnl + first_month_pnl
+    previous_month_pnl = first_month_pnl
+      
+    #loop through rows to find the number of months and 
+    # the total of Profit/Losses column
+    for row in csvreader:
+        current_month = row[0]
+        current_month_pnl = float(row[1]) 
+        #find number of months count
+        total_months = total_months + 1
+        #find total of profit/losses column
+        total_pnl = total_pnl + current_month_pnl
+        
+        #find increase or decrease of monthly profit/loss
+        pnl_increase = current_month_pnl - previous_month_pnl
+        previous_month_pnl = current_month_pnl
+        #find total pnl change        
+        total_pnl_change = total_pnl_change + [pnl_increase]
+        #if statement to extract max and min monthly change in profit/losses
+        if (pnl_increase > max_pnl_increase):
+            max_pnl_increase = pnl_increase
+            max_pnl_increase_month = current_month
+        if (pnl_increase < max_pnl_decrease):
+            max_pnl_decrease = pnl_increase
+            max_pnl_decrease_month = current_month
+    #reset loop and calculate the average change in profit/losses
+        avg_change_pnl = round(sum(total_pnl_change)/len(total_pnl_change), 2)
+#print out analysis           
+    print("Financial Analysis")   
+    print("---------------------------") 
+    print("Total Months:", total_months)
+    print("Total: $", total_pnl)
+    print("Average Change:", avg_change_pnl)
+    print("Greatest Increase in Profits:", max_pnl_increase_month, max_pnl_increase)
+    print("Greatest Decrease in Profits", max_pnl_decrease_month, max_pnl_decrease)
